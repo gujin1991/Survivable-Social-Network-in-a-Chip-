4eaf2b3e74d64a812e18a6f80dfddd01165564f5
congshan <congshanlv@gmail.com>
2016-02-15 17:47:37 -0500

    try use passport.js

--- Sprint1/app.js
+++ Sprint1/app.js
@@ -50,6 +50,48 @@ var dbFile = './history.db';
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database(dbFile);
 
+var passport = require('passport');
+app.use(passport.initialize()); 
+app.use(passport.session()); // persistent login sessions
+var LocalStrategy = require('passport-local').Strategy;
+
+//TODO: TEST PASSPORT.JS AUTHENTICATION
+var crypto = require('crypto');
+function hashPassword(password, salt) {
+    var hash = crypto.createHash('sha256');
+    hash.update(password);
+    hash.update(salt);
+    return hash.digest('hex');
+}
+
+passport.use(new LocalStrategy(function(username, password, done) {
+    db.get('SELECT salt FROM users WHERE username = ?', username, function(err, row) {
+        if (!row) return done(null, false);
+        var hash = hashPassword(password, row.salt);
+        db.get('SELECT username, id FROM users WHERE username = ? AND password = ?', username, hash, function(err, row) {
+            if (!row) return done(null, false);
+            return done(null, row);
+        });
+    });
+}));
+
+passport.serializeUser(function(user, done) {
+    return done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+    db.get('SELECT id, username FROM users WHERE id = ?', id, function(err, row) {
+        if (!row) return done(null, false);
+        return done(null, row);
+    });
+});
+
+// ...
+
+app.post('/signin', passport.authenticate('local', { successRedirect: '/',
+    failureRedirect: '/signin' }));
+// TODO: END OF PASSPORT
+
 io.on('connection', function(socket) {
     var myname;
     socket.on('login', function(username) {
