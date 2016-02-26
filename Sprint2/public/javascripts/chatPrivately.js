717c865bcd30df14ddc670e92c707b746252fa19
Pan <Pan@eternalmisadeMacBook-Pro.local>
2016-02-26 09:48:05 -0500

    update files about Chat Privately Use Case

--- Sprint2/public/javascripts/chatPrivately.js
+++ Sprint2/public/javascripts/chatPrivately.js
@@ -6,12 +6,9 @@ var offlineUserList = {};
 var currentUser = {};
 var chatTarget;
 var socket = io.connect();
-var username = $("#myname").val();
-$("#userlist-dropdown").click(function(event) {
-    console.log("here");
+var $htmlDiv;
+var content = $('.msg');
 
-
-});
 socket.on('connect', function () {
     socket.emit('login',$("#myname").val());
 });
@@ -30,8 +27,8 @@ socket.on('updatelist',function(response){
 // Get the size of an object
     var onlineSize = Object.size(response.online);
     var offlineSize = Object.size(response.offline);
+    var username = response.currentUser.userName;
     sortByName(response.online, function(onlineUserList){
-        console.log(onlineUserList);
         for(var i = 0; i < onlineSize; i++) {
             setDropdownUserlistClick(username, onlineUserList[i].userName,true);
         }
@@ -44,30 +41,111 @@ socket.on('updatelist',function(response){
 });
 
 function setDropdownUserlistClick(currentUser, username, isOnline) {
-    console.log("!!!!!!!!!!!!!!!" + username);
-    var $htmlDiv = $('<li class="disabled"><a href="" id="chat-userList"> '+ currentUser + '</a></li>');
+    $htmlDiv = $('<li class="disabled"><a href="" id="chat-userList"> '+ currentUser + '</a></li>');
     if(isOnline) {
         if (currentUser != username) {
-            var $htmlDiv = $('<li><a href="" id="chat-userList"> '+ username + '</a></li>');
+            $htmlDiv = $('<li><a href="" id="chat-userList"> '+ username + '</a></li>');
         }
     } else {
-        var $htmlDiv = $('<li><a href="" id="chat-userList"> '+ username + '</a></li>');
+        $htmlDiv = $('<li><a href="" id="chat-userList"> '+ username + '</a></li>');
     }
 
     $("#userlist-dropdown-append").append($htmlDiv);
-    $htmlDiv.children('#chat-userlist').click(function (event) {
+    $htmlDiv.children('#chat-userList').click(function() {
         event.preventDefault();
         chatTarget = $(this).text();
-        console.log(chatTarget);
+        alert("Hi, I want to talk with  " + chatTarget);
+        $('#private-head').empty().append('   ' + chatTarget);
+        content.empty();
+        getPrivateMessage(currentUser, chatTarget);
+        $('#post-btn').click(function(event) {
+            event.preventDefault();
+            sendMessage();
+        });
     });
 }
+function getPrivateMessage(senderName, receiverName) {
+    alert("history message");
+    var users = {"sender": senderName, "receiver":receiverName};
+    $.get('/getPrivateHistory', users, function(messages){
+        //Todo: how to display the message received and sent???
+        for(var i=0; i<messages.length; i++) {
+            var message = messages[i];
+            var label = '<div style="color:gray" class="message">' +
+                '<div class="messageHeader">' +
+                '<span>' +
+                '<span>' + message.userName +
+                '</span>' +
+                '<img alt="OK" height="20px" width="20px" style="margin-left: 5px;" src="../images/icons/ok.png">' +
+                '<div class="timestamp pull-right">' +
+                '<i class="fa fa-clock-o fa-1"></i>' +
+                '<small style="margin-left: 5px;">' + message.time + '</small>' +
+                '</div>' +
+                '</span>' +
+                '</div>' +
+                '<div class="messageBody">' +
+                '<strong>'+ message.content +' </strong> '+
+                '</div>' +
+                '</div>';
+            var one = $(label);
+            content.append(one);
+        }
+        $("html, body").animate({ scrollTop: $(document).height() }, 1000);
+        var chat_body = $('#stream-list');
+        var height = chat_body[0].scrollHeight;
+        chat_body.scrollTop(height);
+    });
+}
+function sendMessage(senderName, receiverName) {
+    var text = $('#focusedInput').val();
+    var obj = {};
+    if(text ==="") {
+        swal({
+            title: "Empty input!",
+            text: "You need to write something!",
+            type: "input",
+            showCancelButton: true,
+            closeOnConfirm: false,
+            animation: "slide-from-top",
+            inputPlaceholder: "Write something"
+        }, function(inputValue){
+            if (inputValue === false) return false;
+            if (inputValue === "") {
+                swal.showInputError("You need to write something!");
+                return false
+            }
+            obj['sender'] = senderName;
+            obj['receiver'] = receiverName;
+            obj['text'] = inputValue;
+            console.log("client : "+ senderName + "send to " + receiverName + " a message "+  text);
+            swal.close()
+            //socket.emit('send message',obj);
+            $.post("/sendPrivateMessage",obj,function(){
 
+            });
+        });
+    } else {
+        obj['sender'] = senderName;
+        obj['receiver'] = receiverName;
+        obj['text'] = text;
+        console.log("client : "+ senderName + "send to " + receiverName + " a message "+  text);
+
+        //socket.emit('send message',obj);
+        //call api
+        $.post("/sendPrivateMessage",obj,function(){
+
+        });
+
+        $('#focusedInput').val('');
+        $('#focusedInput').focus();
+    }
+    return false;
+}
 function sortByName(dict, callback) {
     var loweCaseSort = function(a, b) {
         return a.userName.toLowerCase().localeCompare(b.userName.toLowerCase())
     };
     var sorted = [];
-    console.log("sort");
     for(var i = 0; i < dict.length; i++) {
         sorted[sorted.length] = dict[i];
     }
