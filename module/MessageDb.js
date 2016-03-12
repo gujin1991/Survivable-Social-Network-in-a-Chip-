96de8ea3586976c81b8f56179189e38f910a2f1b
guangstick <iamguangyu@gmail.com>
2016-03-12 15:50:13 -0500

    add status column

--- module/MessageDb.js
+++ module/MessageDb.js
@@ -8,13 +8,13 @@ function MessageDb() {
 
     // TODO add status table
 }
-MessageDb.prototype.messageAdd = function (username, message, time, callback) {
+MessageDb.prototype.messageAdd = function (username, message, time, status, callback) {
     //TODO add user exist auth
     var dbtemp = this.db;
     dbtemp.serialize(function () {
-        dbtemp.run("CREATE TABLE IF NOT EXISTS messages (messageId INTEGER PRIMARY KEY AUTOINCREMENT, userName TEXT, time TEXT, content TEXT)");
-        var insertMessage = dbtemp.prepare("insert into messages Values(?, ?,?,?)");
-        insertMessage.run(null, username, time, message);
+        dbtemp.run("CREATE TABLE IF NOT EXISTS messages (messageId INTEGER PRIMARY KEY AUTOINCREMENT, userName TEXT, time TEXT, content TEXT, status TEXT)");
+        var insertMessage = dbtemp.prepare("insert into messages Values(?, ?,?,?, ?)");
+        insertMessage.run(null, username, time, message, status);
         callback(200);
         return;
     });
@@ -22,20 +22,20 @@ MessageDb.prototype.messageAdd = function (username, message, time, callback) {
 
 MessageDb.prototype.getHistory = function (callback) {
     var dbTemp = this.db;
-    dbTemp.serialize(function() {
-        dbTemp.all("SELECT * FROM messages", function(err, rows) {
+    dbTemp.serialize(function () {
+        dbTemp.all("SELECT * FROM messages", function (err, rows) {
             callback(rows);
         })
     });
 };
 
-MessageDb.prototype.privateMessageAdd = function (fromUser, toUser, message, time, callback) {
+MessageDb.prototype.privateMessageAdd = function (fromUser, toUser, message, time, status, callback) {
     //TODO add user exist auth
     var dbtemp = this.db;
     dbtemp.serialize(function () {
-        dbtemp.run("CREATE TABLE IF NOT EXISTS privateMessages (messageId INTEGER PRIMARY KEY AUTOINCREMENT, fromUser TEXT, toUser TEXT, time TEXT, content TEXT)");
-        var insertMessage = dbtemp.prepare("insert into privateMessages Values(?, ?, ?, ?, ?)");
-        insertMessage.run(null, fromUser, toUser, time, message);
+        dbtemp.run("CREATE TABLE IF NOT EXISTS privateMessages (messageId INTEGER PRIMARY KEY AUTOINCREMENT, fromUser TEXT, toUser TEXT, time TEXT, content TEXT, status TEXT)");
+        var insertMessage = dbtemp.prepare("insert into privateMessages Values(?, ?, ?, ?, ?, ?)");
+        insertMessage.run(null, fromUser, toUser, time, message, status);
         callback(200);
         return;
     });
---
+++ test/initDb.js
@@ -0,0 +1,3 @@
+/**
+ * Created by guangyu on 3/12/16.
+ */
