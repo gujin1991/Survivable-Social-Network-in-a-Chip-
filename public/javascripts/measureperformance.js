d0b831da790fb32913c0f7fcd46a7d8f85db9e3d
eternalmisa <panli@andrew.cmu.edu>
2016-03-22 11:35:08 -0400

    no bug version

--- public/javascripts/measureperformance.js
+++ public/javascripts/measureperformance.js
@@ -5,7 +5,6 @@ var value = 0;
 var duration;
 var interval;
 var the_interval;
-var another_interval;
 var the_duration;
 var intervals;
 var count = 0;
@@ -35,6 +34,7 @@ $('#start-btn').click(function() {
         });
         the_interval =  setInterval(testPost, interval);
         the_duration = setTimeout(next,duration/2);
+
         intervals = setInterval(progress, duration/10);
     }
     return false;
@@ -42,27 +42,20 @@ $('#start-btn').click(function() {
 function next() {
     clearInterval(the_interval);
 
-    another_interval = setInterval(testGet, interval);
+    the_interval = setInterval(testGet, interval);
     the_duration = setTimeout(stop,duration/2);
 }
 
 function stop() {
-    clearInterval(another_interval);
+    clearInterval(the_interval);
     $.post('/endMeasurePerformance',function(response){
         console.log(response);
         if (response.statusCode === 411) {
             swal({title: "Error!",text: "Testing Outages!", type: "error", confirmButtonText: "OK" });
         } else {
-<<<<<<< HEAD
-            var time = duration / (interval * 2);
+            var time = duration / 2000;
             document.getElementById('number-of-post').innerHTML = response.postCount / time + "";
             document.getElementById('number-of-get').innerHTML = response.getCount / time + "";
-=======
-
-            document.getElementById('number-of-post').innerHTML = response.postCount * 2 *1000 / duration + "";
-            document.getElementById('number-of-get').innerHTML = response.getCount * 2 * 1000 / duration + "";
-
->>>>>>> 2d24c7b124f9b5065a48f8a4f051f542e956da95
         }
     });
     clear();
@@ -75,40 +68,10 @@ function testPost() {
     var message = {'username': username, 'text': "This is a test message when testing POST: No.'" + count};
     //console.log(message);
     $.post("/testPost",message,function(response){
-
-
         if (response.statusCode === 400) {
             swal({title: "Error!",text: "Cannot get Messages!", type: "error", confirmButtonText: "OK" });
         } else if (response.statusCode === 411) {
-<<<<<<< HEAD
             swal({title: "Error!",text: "Testing Outages!", type: "error", confirmButtonText: "OK" });
-=======
-
-            swal({title: "Error!",text: "Testing Outages!", type: "error", confirmButtonText: "OK" });
-        }else if (response.statusCode === 413) {
-
-            clearInterval(intervals);
-            clearInterval(another_interval);
-            clearInterval(the_interval);
-
-            $.post('/testModeQuit',function(response){
-                if (response.statusCode === 411) {
-                    swal({title: "Error!",text: "Testing Outages!", type: "error", confirmButtonText: "OK" });
-                }
-            });
-
-            $("#progress-bar")
-                .css("width", 0 + "%")
-                .attr("aria-valuenow", 0)
-                .text(0+ "%");
-            clear();
-            document.getElementById('number-of-post').innerHTML = "0";
-            document.getElementById('number-of-get').innerHTML = "0";
-
-            swal({title: "Error!",text: "Post more than Limit !", type: "error", confirmButtonText: "OK" });
-
-
->>>>>>> 2d24c7b124f9b5065a48f8a4f051f542e956da95
         }
     });
 
@@ -134,15 +97,12 @@ function progress() {
 
 $('#stop-btn').click(function() {
     clearInterval(the_interval);
-    clearInterval(another_interval);
     clearInterval(intervals);
-
     $.post('/testModeQuit',function(response){
         if (response.statusCode === 411) {
             swal({title: "Error!",text: "Testing Outages!", type: "error", confirmButtonText: "OK" });
         }
     });
-
     $("#progress-bar")
         .css("width", 0 + "%")
         .attr("aria-valuenow", 0)
