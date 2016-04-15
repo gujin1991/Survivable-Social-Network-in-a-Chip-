224f32a75475f0c9cee14d1f1a20a61536e54282
guangstick <iamguangyu@gmail.com>
2016-04-15 18:25:15 -0400

    remove fse.db, test.db, add database script

---
+++ conf/fse.sql
@@ -0,0 +1,7 @@
+CREATE TABLE IF NOT EXISTS announcements (announcementId INTEGER PRIMARY KEY AUTOINCREMENT, userName TEXT, status TEXT, time TEXT, content TEXT);
+CREATE TABLE IF NOT EXISTS messages (messageId INTEGER PRIMARY KEY AUTOINCREMENT, userName TEXT, time TEXT, content TEXT, status TEXT , nickName TEXT);
+CREATE TABLE IF NOT EXISTS privateMessages (messageId INTEGER PRIMARY KEY AUTOINCREMENT, fromUser TEXT, toUser TEXT, time TEXT, content TEXT, status TEXT, senderNickname TEXT, receiverNickname TEXT);
+CREATE TABLE IF NOT EXISTS testMessages (messageId INTEGER PRIMARY KEY AUTOINCREMENT, userName TEXT, time TEXT, content TEXT, status TEXT);
+CREATE TABLE IF NOT EXISTS users (userName TEXT PRIMARY KEY, password TEXT, joinTime TEXT , status TEXT, privilege TEXT, accountStatus TEXT, nickName TEXT);
+
+insert into users Values("SSNAdmin","admin","1/15/2016, 12:00:00 PM","OK","Administrator","active","");
