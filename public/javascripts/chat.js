01f082fa252a9e505e224cdbb55941f3adf43fc6
eternalmisa <panli@andrew.cmu.edu>
2016-04-17 14:38:40 -0400

    add sweetalert

--- public/javascripts/chat.js
+++ public/javascripts/chat.js
@@ -240,8 +240,8 @@ socket.on('update publicMessage',function(data) {
 
 socket.on('Log out',function() {
 	swal({
-		title: "Sorry! You are out...",
-		text: "You were kicked out by Administrator!",
+		title: "Oops...",
+		text: "Your session has expired. Please log in again!",
 		type: "warning",
 		showCancelButton: false,
 		confirmButtonColor: "#DD6B55",
